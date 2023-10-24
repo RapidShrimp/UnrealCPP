410f885e4b1e7d253fe7cc5f04af37307507b35b
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2023-10-24 16:11:07 +0100

    Advanced Movement Inputs

--- Source/FPSProject/Weapon_Hitscan.cpp
+++ Source/FPSProject/Weapon_Hitscan.cpp
@@ -3,7 +3,8 @@
 
 #include "Weapon_Hitscan.h"
 
-#include "Components/ArrowComponent.h"
+#include "FPSProjectCharacter.h"
+#include "Camera/CameraComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 
@@ -13,20 +14,22 @@ AWeapon_Hitscan::AWeapon_Hitscan()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	_Damage = 10.0f;
-	_Range = 50.0f;
+	_Range = 1000.0f;
 }
 
 bool AWeapon_Hitscan::Fire_Implementation()
 {
-	FVector StartLoc = _Arrow->GetComponentLocation();
-	FVector EndLoc = StartLoc + _Arrow->GetForwardVector() * _Range;
+	UE_LOG(LogTemp,Warning,TEXT("Fire"));
+	FVector StartLoc = OwningCharacter->GetFirstPersonCameraComponent()->GetComponentLocation();
+	FVector EndLoc = StartLoc + OwningCharacter->GetFirstPersonCameraComponent()->GetForwardVector()* _Range;
 	FHitResult Hit;
 	if(UKismetSystemLibrary::LineTraceSingle(GetWorld(),StartLoc,EndLoc,
 	UEngineTypes::ConvertToTraceType(ECC_Visibility),
-	true,{this, GetOwner()},EDrawDebugTrace::ForDuration,
+	true,{this, GetOwner()},EDrawDebugTrace::Persistent,
 	Hit,true,FLinearColor::Red,FLinearColor::Green, 5.0f))
 {
-	UGameplayStatics::ApplyDamage(Hit.GetActor(),_Damage,GetInstigator()->GetController(),this,UDamageType::StaticClass());
+
+	UGameplayStatics::ApplyDamage(Hit.GetActor(),_Damage,OwningCharacter->GetController(),this,UDamageType::StaticClass());
 		return  true;
 }
 	return false;
